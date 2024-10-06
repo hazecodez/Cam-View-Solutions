41258081f6613eb6793fa6fafc54ec1f012be0a9
hazecodez <muhammedhashir298@gmail.com>
2024-10-06 13:51:52 +0530

    About page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import OurServices from "../Components/OurServices";
+
+
+export default function About() {
+  return (
+    <div className="bg-[#ecf3f9]">
+      <Header />
+      <OurServices />
+      <Footer />
+    </div>
+  )
+}
