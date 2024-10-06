2c4dcab6e6e1ce4a4cd7846aa5377f0c9fdcc8a9
hazecodez <muhammedhashir298@gmail.com>
2024-10-06 13:52:23 +0530

    contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,14 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import OurServices from "../Components/OurServices";
 
 
 export default function Contact() {
   return (
-    <div>
-      
+    <div className="bg-[#ecf3f9]">
+      <Header />
+      <OurServices />
+      <Footer />
     </div>
   )
 }
