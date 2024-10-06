bd34c869105e1c258fe947e761924fff1a8493b8
hazecodez <muhammedhashir298@gmail.com>
2024-10-06 13:54:57 +0530

    all routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./Pages/Home";
 import Services from "./Pages/Services";
+import Projects from "./Pages/Projects";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 export default function App() {
   return (
@@ -9,6 +12,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/service" element={<Services/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        
       </Routes>
     </Router>
 
