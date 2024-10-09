4ef2d637e28e266a14735dde483dda7b66f7953d
hazecodez <muhammedhashir298@gmail.com>
2024-10-10 00:31:58 +0530

    Sticky contact transition done

--- src/Components/Whatsapp.jsx
+++ src/Components/Whatsapp.jsx
@@ -6,6 +6,29 @@ export default function Whatsapp() {
   const [click, setClick] = useState(false);
   return (
     <>
+      <div
+        className={`fixed z-50  sm:bottom-8 sm:right-12 bottom-6 right-4 translate-transform duration-500 ${
+          click ? "translate-y-[-160px] opacity-100" : "translate-y-0 opacity-0"
+        }`}
+      >
+        <a href="https://wa.link/755bhf">
+          <div className="flex justify-end bg-green-800 p-3 hover:transition-colors hover:bg-blue-500 text-slate-50 shadow-lg rounded-full ">
+            <FaWhatsapp className="w-7 h-7 " />
+          </div>
+        </a>
+      </div>
+      <div
+        className={`fixed z-50  sm:bottom-8 sm:right-12 bottom-6 right-4 translate-transform duration-500 ${
+          click ? "translate-y-[-80px] opacity-100" : "translate-y-0 opacity-0"
+        }`}
+      >
+        <a href="tel:918606548795">
+          <div className="flex justify-end bg-green-800 p-3 hover:transition-colors hover:bg-blue-500 text-slate-50 shadow-lg rounded-full ">
+            <FaPhone className="w-7 h-7 " />
+          </div>
+        </a>
+      </div>
+
       <div className="fixed z-50  bottom-6 right-4  sm:bottom-8 sm:right-12">
         <a onClick={() => setClick(!click)}>
           <div
@@ -21,24 +44,6 @@ export default function Whatsapp() {
           </div>
         </a>
       </div>
-      {click && (
-        <>
-          <div className="fixed z-50  bottom-6 right-24  sm:bottom-8 sm:right-28">
-            <a href="https://wa.link/755bhf">
-              <div className="flex justify-end bg-green-800 p-3 text-slate-50 shadow-lg rounded-full ">
-                <FaWhatsapp className="w-7 h-7 " />
-              </div>
-            </a>
-          </div>
-          <div className="fixed z-50  bottom-24 right-4  sm:bottom-24 sm:right-12">
-            <a href="tel:918606548795">
-              <div className="flex justify-end bg-green-800 p-3 text-slate-50 shadow-lg rounded-full ">
-                <FaPhone className="w-7 h-7 " />
-              </div>
-            </a>
-          </div>
-        </>
-      )}
     </>
   );
 }
