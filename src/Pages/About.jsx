afe693a592526e741bd02cce5fcf35be9e8ca11b
hazecodez <muhammedhashir298@gmail.com>
2024-11-02 20:24:27 +0530

    -

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div className="bg-[#ecf3f9]">
       <Header />
-      <div className="container px-4 lg:px-8 mx-auto max-w-screen-xl text-gray-700 overflow-y-hidden overflow-x-hidden">
+      <div className="container px-4 lg:px-8 mx-auto max-w-screen-xl text-gray-700 overflow-y-hidden overflow-x-hidden ">
         <AboutCam />
       </div>
       <Footer />
